80295a0f3642ef2f9f7120d5b8f38ae967e2ba8c
Ahmed Muse <ahmeddove114@gmail.com>
2021-09-26 17:16:28 +0300

    invoicing system now working

--- invoice/urls.py
+++ invoice/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
 	path('clients/', views.clients, name="clients"),
 
 	path('creat-invoice', views.createInvoice, name="create-nvoice"),# I have added invoices
-	path('create-build-invoice/<slug:slug>', views.createBuildInvoice, name="create-build-invoice"),
+	path('create-build/<slug:slug>', views.createBuildInvoice, name="create-build-invoice"),
 	#path('invoices/create-build/<slug:slug>',views.createBuildInvoice, name='create-build-invoice'),
 
-
 	#Delete an invoice
-	path('invoices/delete/<slug:slug>',views.deleteInvoice, name='delete-invoice'),
+	path('delete/<slug:slug>',views.deleteInvoice, name='delete-invoice'),
 	path('invoices/view-pdf/<slug:slug>',views.viewPDFInvoice, name='view-pdf-invoice'),
-	
+	#################################################################333
+
 	
 ] 
